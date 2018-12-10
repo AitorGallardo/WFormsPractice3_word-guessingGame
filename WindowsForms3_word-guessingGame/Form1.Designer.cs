e59832048b921ee8de3a4d1ce1afce9afd2854c8
aitorgit <tugasmec@gmail.com>
2018-12-10 17:37:23 +0100

    v

--- WindowsForms3_word-guessingGame/Form1.Designer.cs
+++ WindowsForms3_word-guessingGame/Form1.Designer.cs
@@ -32,21 +32,20 @@
             this.startGame = new System.Windows.Forms.Button();
             this.tabControlGame = new System.Windows.Forms.TabControl();
             this.Configuracio = new System.Windows.Forms.TabPage();
-            this.passwordLabel = new System.Windows.Forms.Label();
-            this.userLabel = new System.Windows.Forms.Label();
-            this.passwordTxtBox = new System.Windows.Forms.TextBox();
-            this.userTxtBox = new System.Windows.Forms.TextBox();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.complexesTxtBox = new System.Windows.Forms.TextBox();
             this.ComplexLabel = new System.Windows.Forms.Label();
+            this.normalsTxtBox = new System.Windows.Forms.TextBox();
+            this.complexesListBox = new System.Windows.Forms.ListBox();
+            this.simplesListBox = new System.Windows.Forms.ListBox();
+            this.simplesTxtBox = new System.Windows.Forms.TextBox();
             this.normalLabel = new System.Windows.Forms.Label();
             this.simpleLabel = new System.Windows.Forms.Label();
-            this.listBox2 = new System.Windows.Forms.ListBox();
-            this.listBox3 = new System.Windows.Forms.ListBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.normalsListBox = new System.Windows.Forms.ListBox();
             this.Joc = new System.Windows.Forms.TabPage();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.passwordTxtBox = new System.Windows.Forms.TextBox();
             this.tabControlGame.SuspendLayout();
             this.Configuracio.SuspendLayout();
-            this.panel1.SuspendLayout();
             this.Joc.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -84,14 +83,19 @@
             this.tabControlGame.SelectedIndex = 0;
             this.tabControlGame.Size = new System.Drawing.Size(776, 426);
             this.tabControlGame.TabIndex = 3;
+            this.tabControlGame.SelectedIndexChanged += new System.EventHandler(this.tabControlGame_SelectedIndexChanged);
             // 
             // Configuracio
             // 
-            this.Configuracio.Controls.Add(this.passwordLabel);
-            this.Configuracio.Controls.Add(this.userLabel);
-            this.Configuracio.Controls.Add(this.passwordTxtBox);
-            this.Configuracio.Controls.Add(this.userTxtBox);
-            this.Configuracio.Controls.Add(this.panel1);
+            this.Configuracio.Controls.Add(this.complexesTxtBox);
+            this.Configuracio.Controls.Add(this.ComplexLabel);
+            this.Configuracio.Controls.Add(this.normalsTxtBox);
+            this.Configuracio.Controls.Add(this.complexesListBox);
+            this.Configuracio.Controls.Add(this.simplesListBox);
+            this.Configuracio.Controls.Add(this.simplesTxtBox);
+            this.Configuracio.Controls.Add(this.normalLabel);
+            this.Configuracio.Controls.Add(this.simpleLabel);
+            this.Configuracio.Controls.Add(this.normalsListBox);
             this.Configuracio.Location = new System.Drawing.Point(4, 29);
             this.Configuracio.Name = "Configuracio";
             this.Configuracio.Padding = new System.Windows.Forms.Padding(3);
@@ -100,66 +104,74 @@
             this.Configuracio.Text = "Configuració";
             this.Configuracio.UseVisualStyleBackColor = true;
             // 
-            // passwordLabel
+            // complexesTxtBox
             // 
-            this.passwordLabel.AutoSize = true;
-            this.passwordLabel.Location = new System.Drawing.Point(295, 79);
-            this.passwordLabel.Name = "passwordLabel";
-            this.passwordLabel.Size = new System.Drawing.Size(78, 20);
-            this.passwordLabel.TabIndex = 7;
-            this.passwordLabel.Text = "Password";
-            this.passwordLabel.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // userLabel
-            // 
-            this.userLabel.AutoSize = true;
-            this.userLabel.Location = new System.Drawing.Point(302, 23);
-            this.userLabel.Name = "userLabel";
-            this.userLabel.Size = new System.Drawing.Size(43, 20);
-            this.userLabel.TabIndex = 6;
-            this.userLabel.Text = "User";
-            // 
-            // passwordTxtBox
-            // 
-            this.passwordTxtBox.Location = new System.Drawing.Point(273, 102);
-            this.passwordTxtBox.Name = "passwordTxtBox";
-            this.passwordTxtBox.Size = new System.Drawing.Size(153, 26);
-            this.passwordTxtBox.TabIndex = 5;
-            // 
-            // userTxtBox
-            // 
-            this.userTxtBox.Location = new System.Drawing.Point(273, 46);
-            this.userTxtBox.Name = "userTxtBox";
-            this.userTxtBox.Size = new System.Drawing.Size(153, 26);
-            this.userTxtBox.TabIndex = 4;
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.ComplexLabel);
-            this.panel1.Controls.Add(this.normalLabel);
-            this.panel1.Controls.Add(this.simpleLabel);
-            this.panel1.Controls.Add(this.listBox2);
-            this.panel1.Controls.Add(this.listBox3);
-            this.panel1.Controls.Add(this.listBox1);
-            this.panel1.Location = new System.Drawing.Point(0, 180);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(768, 213);
-            this.panel1.TabIndex = 3;
-            this.panel1.Visible = false;
+            this.complexesTxtBox.Location = new System.Drawing.Point(554, 174);
+            this.complexesTxtBox.Name = "complexesTxtBox";
+            this.complexesTxtBox.Size = new System.Drawing.Size(120, 26);
+            this.complexesTxtBox.TabIndex = 8;
+            this.complexesTxtBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.allInputTxtBox_KeyPress);
             // 
             // ComplexLabel
             // 
             this.ComplexLabel.AutoSize = true;
-            this.ComplexLabel.Location = new System.Drawing.Point(604, 100);
+            this.ComplexLabel.Location = new System.Drawing.Point(564, 140);
             this.ComplexLabel.Name = "ComplexLabel";
             this.ComplexLabel.Size = new System.Drawing.Size(101, 20);
             this.ComplexLabel.TabIndex = 5;
             this.ComplexLabel.Text = "P.Complexes";
             // 
+            // normalsTxtBox
+            // 
+            this.normalsTxtBox.Location = new System.Drawing.Point(295, 174);
+            this.normalsTxtBox.Name = "normalsTxtBox";
+            this.normalsTxtBox.Size = new System.Drawing.Size(120, 26);
+            this.normalsTxtBox.TabIndex = 7;
+            this.normalsTxtBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.allInputTxtBox_KeyPress);
+            // 
+            // complexesListBox
+            // 
+            this.complexesListBox.FormattingEnabled = true;
+            this.complexesListBox.ItemHeight = 20;
+            this.complexesListBox.Items.AddRange(new object[] {
+            "GRINYOLAR",
+            "ESQUERDAR",
+            "ESBIAXAR",
+            "AXAFLANAR"});
+            this.complexesListBox.Location = new System.Drawing.Point(554, 53);
+            this.complexesListBox.Name = "complexesListBox";
+            this.complexesListBox.Size = new System.Drawing.Size(120, 84);
+            this.complexesListBox.TabIndex = 2;
+            // 
+            // simplesListBox
+            // 
+            this.simplesListBox.FormattingEnabled = true;
+            this.simplesListBox.ItemHeight = 20;
+            this.simplesListBox.Items.AddRange(new object[] {
+            "CEC",
+            "CRANC",
+            "DEU",
+            "MES",
+            "FIL",
+            "MEU",
+            "COTXE"});
+            this.simplesListBox.Location = new System.Drawing.Point(43, 53);
+            this.simplesListBox.Name = "simplesListBox";
+            this.simplesListBox.Size = new System.Drawing.Size(120, 84);
+            this.simplesListBox.TabIndex = 0;
+            // 
+            // simplesTxtBox
+            // 
+            this.simplesTxtBox.Location = new System.Drawing.Point(43, 174);
+            this.simplesTxtBox.Name = "simplesTxtBox";
+            this.simplesTxtBox.Size = new System.Drawing.Size(120, 26);
+            this.simplesTxtBox.TabIndex = 6;
+            this.simplesTxtBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.allInputTxtBox_KeyPress);
+            // 
             // normalLabel
             // 
             this.normalLabel.AutoSize = true;
-            this.normalLabel.Location = new System.Drawing.Point(336, 100);
+            this.normalLabel.Location = new System.Drawing.Point(308, 140);
             this.normalLabel.Name = "normalLabel";
             this.normalLabel.Size = new System.Drawing.Size(81, 20);
             this.normalLabel.TabIndex = 4;
@@ -168,17 +180,17 @@
             // simpleLabel
             // 
             this.simpleLabel.AutoSize = true;
-            this.simpleLabel.Location = new System.Drawing.Point(39, 100);
+            this.simpleLabel.Location = new System.Drawing.Point(61, 140);
             this.simpleLabel.Name = "simpleLabel";
             this.simpleLabel.Size = new System.Drawing.Size(79, 20);
             this.simpleLabel.TabIndex = 3;
             this.simpleLabel.Text = "P.Simples";
             // 
-            // listBox2
+            // normalsListBox
             // 
-            this.listBox2.FormattingEnabled = true;
-            this.listBox2.ItemHeight = 20;
-            this.listBox2.Items.AddRange(new object[] {
+            this.normalsListBox.FormattingEnabled = true;
+            this.normalsListBox.ItemHeight = 20;
+            this.normalsListBox.Items.AddRange(new object[] {
             "LLAPIS",
             "RATOLI",
             "GRIPAU",
@@ -186,44 +198,15 @@
             "CADIRA",
             "TAULA",
             "COTXE"});
-            this.listBox2.Location = new System.Drawing.Point(306, 13);
-            this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(120, 84);
-            this.listBox2.TabIndex = 1;
-            // 
-            // listBox3
-            // 
-            this.listBox3.FormattingEnabled = true;
-            this.listBox3.ItemHeight = 20;
-            this.listBox3.Items.AddRange(new object[] {
-            "GRINYOLAR",
-            "ESQUERDAR",
-            "ESBIAXAR",
-            "AXAFLANAR"});
-            this.listBox3.Location = new System.Drawing.Point(595, 13);
-            this.listBox3.Name = "listBox3";
-            this.listBox3.Size = new System.Drawing.Size(120, 84);
-            this.listBox3.TabIndex = 2;
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 20;
-            this.listBox1.Items.AddRange(new object[] {
-            "CEC",
-            "CRANC",
-            "DEU",
-            "MES",
-            "FIL",
-            "MEU",
-            "COTXE"});
-            this.listBox1.Location = new System.Drawing.Point(25, 13);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 84);
-            this.listBox1.TabIndex = 0;
+            this.normalsListBox.Location = new System.Drawing.Point(295, 53);
+            this.normalsListBox.Name = "normalsListBox";
+            this.normalsListBox.Size = new System.Drawing.Size(120, 84);
+            this.normalsListBox.TabIndex = 1;
             // 
             // Joc
             // 
+            this.Joc.Controls.Add(this.passwordLabel);
+            this.Joc.Controls.Add(this.passwordTxtBox);
             this.Joc.Controls.Add(this.word_types);
             this.Joc.Controls.Add(this.startGame);
             this.Joc.Location = new System.Drawing.Point(4, 29);
@@ -233,6 +216,24 @@
             this.Joc.TabIndex = 1;
             this.Joc.Text = "Joc";
             this.Joc.UseVisualStyleBackColor = true;
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(17, 15);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(78, 20);
+            this.passwordLabel.TabIndex = 8;
+            this.passwordLabel.Text = "Password";
+            // 
+            // passwordTxtBox
+            // 
+            this.passwordTxtBox.Location = new System.Drawing.Point(6, 38);
+            this.passwordTxtBox.MaxLength = 10;
+            this.passwordTxtBox.Name = "passwordTxtBox";
+            this.passwordTxtBox.Size = new System.Drawing.Size(100, 26);
+            this.passwordTxtBox.TabIndex = 2;
+            this.passwordTxtBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.allInputTxtBox_KeyPress);
             // 
             // Form1
             // 
@@ -245,9 +246,8 @@
             this.tabControlGame.ResumeLayout(false);
             this.Configuracio.ResumeLayout(false);
             this.Configuracio.PerformLayout();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             this.Joc.ResumeLayout(false);
+            this.Joc.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -258,18 +258,18 @@
         private System.Windows.Forms.Button startGame;
         private System.Windows.Forms.TabControl tabControlGame;
         private System.Windows.Forms.TabPage Configuracio;
-        private System.Windows.Forms.Label passwordLabel;
-        private System.Windows.Forms.Label userLabel;
-        private System.Windows.Forms.TextBox passwordTxtBox;
-        private System.Windows.Forms.TextBox userTxtBox;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.ListBox listBox2;
-        private System.Windows.Forms.ListBox listBox3;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox normalsListBox;
+        private System.Windows.Forms.ListBox complexesListBox;
+        private System.Windows.Forms.ListBox simplesListBox;
         private System.Windows.Forms.TabPage Joc;
         private System.Windows.Forms.Label ComplexLabel;
         private System.Windows.Forms.Label normalLabel;
         private System.Windows.Forms.Label simpleLabel;
+        private System.Windows.Forms.TextBox complexesTxtBox;
+        private System.Windows.Forms.TextBox normalsTxtBox;
+        private System.Windows.Forms.TextBox simplesTxtBox;
+        private System.Windows.Forms.TextBox passwordTxtBox;
+        private System.Windows.Forms.Label passwordLabel;
     }
 }
 
--- WindowsForms3_word-guessingGame/Form1.cs
+++ WindowsForms3_word-guessingGame/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,9 +16,14 @@ namespace WindowsForms3_word_guessingGame
         // List<String> simpleWords = new List<String>() {"CEC", "CRANC", "DEU", "MES", "FIL", "MEU", "COTXE"};
         // List<String> normalWords = new List<String>() { "LLAPIS", "RATOLI", "GRIPAU", "MOTOS", "CADIRA", "TAULA", "COTXE" };
         // List<String> complexWords = new List<String>() { "GRINYOLAR", "ESQUERDAR", "ESBIAXAR", "AXAFLANAR"};
+        String correctPassword = "ABCD1234";
+        Regex regexSimple = new Regex("^[A-Z]{1,5}$");
+        Regex regexNormal = new Regex("^[A-Z]{6,9}$");
+        Regex regexComplex = new Regex("^[A-Z]{7,15}$");
         public Form1()
         {
             InitializeComponent();
+            tabControlGame.TabPages.Remove(Configuracio);
         }
 
         private void startGame_Click(object sender, EventArgs e)
@@ -26,11 +32,87 @@ namespace WindowsForms3_word_guessingGame
             {
             }
         }
-
-        private void label2_Click(object sender, EventArgs e)
+        // CONFIG
+        private bool logIn(String password)
         {
-
+            if (this.correctPassword.Equals(password))
+            {
+                tabControlGame.TabPages.Add(Configuracio);
+                tabControlGame.SelectedTab = Configuracio;
+                // configPasswordPanel.Visible = false;
+                // configWordPanel.Visible = true;
+                //768; 393
+                // configWordPanel.Size = new Size(768, 393);
+                // configWordPanel.AutoSize = true;
+                return true;
+            }
+            return false;
         }
 
+        private void allInputTxtBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBox txtBx = (TextBox)sender;
+            if(e.KeyChar == Convert.ToChar(Keys.Return))
+            {
+                switch (txtBx.Name)
+                {
+                    case "passwordTxtBox":
+                        logIn(passwordTxtBox.Text);
+                        txtBx.Clear();
+                        break;
+                    case "simplesTxtBox":
+                        if (regexSimple.IsMatch(txtBx.Text))
+                        {
+                            simplesListBox.Items.Add(txtBx.Text);
+                            MessageBox.Show("La paraula s'ha afegit correctament!");
+                            txtBx.Clear();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Paraula incorrecte. Les normes per introduir una paraula del tipus simple són: " +
+                                            "\n\n# Nomès caràcters en majúscula\n# Sense accents\n# Paraules de 1 a 5 caràcters");
+                            txtBx.Clear();
+                        }
+                        break;
+                    case "normalsTxtBox":
+                        if (regexNormal.IsMatch(txtBx.Text))
+                        {
+                            normalsListBox.Items.Add(txtBx.Text);
+                            MessageBox.Show("La paraula s'ha afegit correctament!");
+                            txtBx.Clear();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Paraula incorrecte. Les normes per introduir una paraula del tipus simple són: " +
+                                            "\n\n# Nomès caràcters en majúscula\n# Sense accents\n# Paraules de 6 a 9 caràcters");
+                            txtBx.Clear();
+                        }
+                        break;
+                    case "complexesTxtBox":
+                        if (regexNormal.IsMatch(txtBx.Text))
+                        {
+                            complexesListBox.Items.Add(txtBx.Text);
+                            MessageBox.Show("La paraula s'ha afegit correctament!");
+                            txtBx.Clear();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Paraula incorrecte. Les normes per introduir una paraula del tipus simple són: " +
+                                            "\n\n# Nomès caràcters en majúscula\n# Sense accents\n# Paraules de 7 a 15 caràcters");
+                            txtBx.Clear();
+                        }
+                        break;
+                }              
+            }
+        }
+
+        private void tabControlGame_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            if (tabControlGame.SelectedTab != Configuracio)
+            {
+                tabControlGame.TabPages.Remove(Configuracio);
+            }
+        }
     }
 }
